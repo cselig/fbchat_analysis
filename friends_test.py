4ca0ebf80f398c5d754aa463649e1bc1c5f523f3
cselig <Christian@Christians-MacBook-Air.local>
2017-03-19 22:45:07 -0700

    Remove some files

--- friends_test.py
+++
@@ -1,6 +0,0 @@
-import facebook
-
-graph = facebook.GraphAPI(access_token)
-friends = graph.get_object("me/friends")
-for friend in friends['data']:
-    print "{0} has id {1}".format(friend['name'].encode('utf-8'), friend['id'])
